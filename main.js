58c6bc4c62733c50ba8e10a8960ebbbec5aafe47
Andrew Wang <88258825+andwan55@users.noreply.github.com>
2023-01-19 16:06:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function popup(imgid){ // function to display popup image of clicked image
+function popup(imgsrc){ // function to display popup image of clicked image
     // Get modal
     var modal = document.getElementById("myModal");
 
     // Get image and insert it inside modal
     var modalImg = document.getElementById("img");
     modal.style.display = "block";
-    modalImg.src = document.getElementById(imgid).src;
+    modalImg.src = imgsrc;
 
     // <span> element to close the modal
     var span = document.getElementsByClassName("close")[0];
